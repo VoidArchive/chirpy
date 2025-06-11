ffb719a4074de74fb07dfc58a65c1cfb122c82f4
voidarchive <voidarchived@gmail.com>
2025-06-11 17:23:43 +0545

    L11: Custom Handlers Healthz

--- main.go
+++ main.go
@@ -4,11 +4,23 @@ import "net/http"
 
 func main() {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", healthzHandler)
+
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
 
 	server := &http.Server{
-		Handler: mux,
 		Addr:    ":8080",
+		Handler: mux,
 	}
 
 	server.ListenAndServe()
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+	w.WriteHeader(http.StatusOK)
+
+	w.Write([]byte("OK"))
 }
